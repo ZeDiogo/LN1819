ef65346a67083749b42c9bffd7a9db0e92403ca0
ZeDiogo <zediogooliveira@hotmail.com>
2018-10-03 19:09:13 +0100

    alinea a) done & tested

--- run.sh
+++ run.sh
@@ -2,11 +2,30 @@
 
 rm -f *.fst *.pdf
 
-# mmm2mm.fst
-fstcompile --isymbols=syms.txt --osymbols=syms.txt --keep_isymbols --keep_osymbols   mmm2mm.txt   | fstarcsort >  mmm2mm.fst
+### mmm2mm.fst
+fstcompile --isymbols=syms.txt --osymbols=syms.txt --keep_isymbols --keep_osymbols   mmm2mm.txt   | fstarcsort >  mmm2mmBef.fst
+fstinvert  mmm2mmBef.fst > mmm2mm.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait mmm2mm.fst | dot -Tpdf  > mmm2mm.pdf
-echo "Transdutor que converte meses de portugues abreviado para numeros no formato MM"
-fstrmepsilon mmm2mm.fst | fsttopsort | fstprint --isymbols=syms-out.txt
-echo " "
+# echo "Transdutor que converte meses de portugues abreviado para numeros no formato MM"
+# fstrmepsilon mmm2mm.fst | fsttopsort | fstprint --isymbols=syms-out.txt
+# echo " "
 
+### convert days to days
+fstcompile --isymbols=syms.txt --osymbols=syms.txt --keep_isymbols --keep_osymbols   day2day.txt   | fstarcsort >  day2day.fst
+# fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait day2day.fst | dot -Tpdf  > day2day.pdf
 
+### convert year to year
+fstcompile --isymbols=syms.txt --osymbols=syms.txt --keep_isymbols --keep_osymbols   year2year.txt   | fstarcsort >  year2year.fst
+# fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait year2year.fst | dot -Tpdf  > year2year.pdf
+
+### convert / to /
+fstcompile --isymbols=syms.txt --osymbols=syms.txt --keep_isymbols --keep_osymbols   bar.txt   | fstarcsort >  bar.fst
+# fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait bar.fst | dot -Tpdf  > bar.pdf
+
+### convert misto2numerico
+fstconcat day2day.fst bar.fst > 1.fst
+fstconcat 1.fst mmm2mm.fst > 2.fst
+fstconcat 2.fst bar.fst > 3.fst
+fstconcat 3.fst year2year.fst > misto2numerico.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait misto2numerico.fst | dot -Tpdf  > misto2numerico.pdf
+rm -f 1.fst 2.fst 3.fst
--- test.sh
+++ test.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 # TESTING
-# SET
-fstcompile --isymbols=syms.txt --osymbols=syms.txt --keep_isymbols --keep_osymbols   testSET.txt   | fstarcsort >  testSET.fst
-fstcompose testSET.fst mmm2mm.fst > resSET.fst
+# mmm2mm
+fstcompile --isymbols=syms.txt --osymbols=syms.txt --keep_isymbols --keep_osymbols   testmmm2mm.txt   | fstarcsort >  testmmm2mm.fst
+fstcompose testmmm2mm.fst mmm2mm.fst > resmmm2mm.fst
 echo -n "O resultado da conversao de SET em numero: "
-fstproject --project_output resSET.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt | awk '{print $3}	'
+fstproject --project_output resmmm2mm.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt | awk '{print $3}	' ORS=''
+# fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait resSET.fst | dot -Tpdf  > resSET.pdf
+rm -f testmmm2mm.fst resmmm2mm.fst
+echo ""
+
+# misto2numerico
+fstcompile --isymbols=syms.txt --osymbols=syms.txt --keep_isymbols --keep_osymbols   testMisto2numerico.txt   | fstarcsort >  testMisto2numerico.fst
+fstcompose testMisto2numerico.fst misto2numerico.fst > resMisto2numerico.fst
+echo -n "O resultado da conversao de 15/SET/2018: "
+fstproject --project_output resMisto2numerico.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt | awk '{print $3}	' ORS=''
+rm -f testMisto2numerico.fst resMisto2numerico.fst
+echo ""
