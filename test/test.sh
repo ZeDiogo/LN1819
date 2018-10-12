d6e301e58b35b76ab0da18fed17441faa49f9bfe
ZeDiogo <zediogooliveira@hotmail.com>
2018-10-12 15:49:28 +0100

    finished test script and input & output for the delivery

--- test/test.sh
+++ test/test.sh
@@ -5,8 +5,21 @@ SYMBOLS="../syms.txt"
 SYMBOLS_OUT="../syms-out.txt"
 TRANSDUTORS_DIR="../"
 TESTS_FILE="tests.txt"
+INPUT_FILE="input.txt"
 
 
+### Generate Transductors from input INPUT_FILE
+IFS=$'\n'
+set -f
+for i in $(cat < "$INPUT_FILE"); do
+	name=$(cut -d ';' -f1 <<< $i)
+	input=$(cut -d ';' -f2 <<< $i)
+	sudo python word2fst.py -s $SYMBOLS $input > "$name"".txt"
+	fstcompile --isymbols=$SYMBOLS --osymbols=$SYMBOLS --keep_isymbols --keep_osymbols   "$name"".txt"   | fstarcsort >  "$name"".fst"
+	rm -f "$name"".txt"
+done
+
+### Parse input transductor, 
 testCounter=0
 IFS=$'\n'       # make newlines the only separator
 set -f          # disable globbing
@@ -20,14 +33,15 @@ done
 for ((i=0;i<$testCounter;i++)); do
 	#Make script convert input into a transductor
 	TEST_NAME="test_""${TRANSDUCTOR[$i]}"
-	RES_NAME="res_""${TRANSDUCTOR[$i]}"
-	python word2fst.py -s $SYMBOLS ${INPUT[$i]} > "$TEST_NAME"".txt"
-	fstcompile --isymbols=$SYMBOLS --osymbols=$SYMBOLS --keep_isymbols --keep_osymbols   "$TEST_NAME"".txt"   | fstarcsort >  "$TEST_NAME"".fst"
-	fstcompose "$TEST_NAME"".fst" "$TRANSDUTORS_DIR""${TRANSDUCTOR[$i]}"".fst" > "$RES_NAME"".fst"
+	number=$(cut -d '_' -f1 <<< ${INPUT[$i]})
+	RES_NAME="$number""_""${TRANSDUCTOR[$i]}"
+
+	fstcompose "${INPUT[$i]}"".fst" "$TRANSDUTORS_DIR""${TRANSDUCTOR[$i]}"".fst" > "$RES_NAME"".fst"
 	OUT=$(fstproject --project_output "$RES_NAME"".fst" | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=$SYMBOLS_OUT | awk '{print $3}	' ORS='')
 	OUT_PROCESSED=$(echo $OUT | sed -e 's/_/ /g' | xargs)	
 	if [ "$OUT_PROCESSED" == "${OUTPUT[$i]}" ]; then 
-		echo "PASSED TEST $((i+1)) "
+		echo "PASSED TEST $((i+1))"
+		echo "Converted "
 	else
 		echo "FAILED TEST $((i+1))"
 		echo "EXPECTED: ${OUTPUT[$i]}"
